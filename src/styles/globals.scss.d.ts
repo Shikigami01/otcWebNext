2a14d87b55700279a6694d9369084d94295f1aa5
houraijin <contact@overthecoffee.net>
2023-11-06 16:39:36 +0900

    feat: 🎸 global.scssの追加

---
+++ src/styles/globals.scss.d.ts
@@ -0,0 +1,16 @@
+export type Styles = {
+  header: string;
+  rhap_container: string;
+  'rhap_controls-section': string;
+  'rhap_main-controls-button': string;
+  'rhap_progress-indicator': string;
+  rhap_time: string;
+  'rhap_volume-button': string;
+  'rhap_volume-indicator': string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
