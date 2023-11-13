cf84d23de88bcde32957226d23fb4c381b301a85
houraijin <contact@overthecoffee.net>
2023-11-14 01:43:22 +0900

    refactor: 💡 オーディオプレイヤーのコンポーネントの修正

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,21 +1,28 @@
-import AudioPlayer from "react-h5-audio-player";
-import "react-h5-audio-player/lib/styles.css";
+'use client';
+
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 type Props = {
   id: number;
+  isListen: boolean;
 };
 
-export const Player: React.FC<Props> = ({ id }) => {
-  const src = `/audio/${String(id).padStart(4, "0")}.mp3`;
+export const Player: React.FC<Props> = ({ id, isListen }) => {
+  const src = `/audio/${String(id).padStart(4, '0')}.m4a`;
 
   return (
-    <AudioPlayer
-      src={src}
-      onPlay={(e) => console.log("onPlay")}
-      volume={0.2}
-      showJumpControls={false}
-      autoPlayAfterSrcChange={false}
-      customAdditionalControls={[]}
-    />
+    <>
+      {isListen && (
+        <AudioPlayer
+          src={src}
+          onPlay={(e) => console.log('onPlay')}
+          volume={0.2}
+          showJumpControls={false}
+          autoPlayAfterSrcChange={false}
+          customAdditionalControls={[]}
+        />
+      )}
+    </>
   );
 };
